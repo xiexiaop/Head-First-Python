cefbd9929006f76fa07bfe52091ebdfecdc71e0f
Zac <13947123462@163.com>
2019-06-09 22:48:34 +0800

    20190609
    微信公众号发送消息至用户：
    1.查找appid以及appsecret
    2.维护IP白名单获得token
    3.问题:发送消息无权限

---
+++ WeChat Integration/2_subscriberSendMsg.py
@@ -0,0 +1,51 @@
+# pip3 install requests
+import requests
+import json
+
+
+def get_access_token():
+    """
+    获取微信全局接口的凭证(默认有效期俩个小时)
+    如果不每天请求次数过多, 通过设置缓存即可
+    """
+    result = requests.get(
+        url="https://api.weixin.qq.com/cgi-bin/token",
+        params={
+            "grant_type": "client_credential",
+            "appid": "wx3a026a0fbe15d5cb",
+            "secret": "90fe03cc0d1680ef21dcae12f4d0b762",
+        }
+    ).json()
+
+    if result.get("access_token"):
+        access_token = result.get('access_token')
+    else:
+        access_token = None
+    return access_token
+
+
+def sendmsg(openid, msg):
+
+    access_token = get_access_token()
+
+    body = {
+        "touser": openid,
+        "msgtype": "text",
+        "text": {
+            "content": msg
+        }
+    }
+    response = requests.post(
+        url="https://api.weixin.qq.com/cgi-bin/message/custom/send",
+        params={
+            'access_token': access_token
+        },
+        data=bytes(json.dumps(body, ensure_ascii=False), encoding='utf-8')
+    )
+    # 这里可根据回执code进行判定是否发送成功(也可以根据code根据错误信息)
+    result = response.json()
+    print(result)
+
+
+if __name__ == '__main__':
+    sendmsg('personality19900901', '发送消息内容')
