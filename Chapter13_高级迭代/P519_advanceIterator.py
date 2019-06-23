2ab0883f903d22b6c065735186023a1de5a5b92f
Zac <13947123462@163.com>
2019-06-23 10:51:39 +0800

    20190623
    Chapter13 Advanced Iterator

---
+++ Chapter13_高级迭代/P519_advanceIterator.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import pprint
+
+
+def convert2ampm(time24: str) -> str:
+    return datetime.strptime(time24, '%H:%M').strftime('%I:%M%p')
+
+
+with open('buzzers.csv') as data:
+    ignore = data.readline()
+    flights = {}
+    for line in data:
+        k, v = line.strip().split(',')
+        flights[k] = v
+
+pprint.pprint(flights)
+
+
+# conprehesion
+fts = { convert2ampm(k):v.title()
+        for k,v in flights.items()
+}
+
+pprint.pprint(fts)
+print()
+
+# destination as key, times as a value of list
+when = {dest: [ k for k,v in fts.items() if v == dest]
+        for dest in set(fts.values())}
+pprint.pprint(when)
