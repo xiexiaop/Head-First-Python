601c1ebe7ba5696f152a4809fb4c1c0e7a5e98e2
Zac <13947123462@163.com>
2019-07-01 22:06:27 +0800

    20190701
    解析wav文件

--- baiduYuyin integration/1.识别pcm文件/indentifyVoiceFile.py
+++ baiduYuyin integration/1.识别pcm文件/indentifyVoiceFile.py
@@ -3,4 +3,7 @@ import wave
 
 path = "16k.wav"
 with wave.open(path, "rb") as f:
-    print(f.getparams())
+    # print(f.getparams())
+    frame = f.readframes(64)
+    print(frame)
+
--- baiduYuyin integration/2.调用百度语音接口识别pcm文件/baiduyuyin.py
+++ baiduYuyin integration/2.调用百度语音接口识别pcm文件/baiduyuyin.py
@@ -1,6 +1,8 @@
 
+import wave
 import requests
 import json
+import base64
 
 # Get Access Token
 url = "https://openapi.baidu.com/oauth/2.0/token"
@@ -10,16 +12,32 @@ r = requests.get(url, params=params)
 jsonObject = json.loads(r.text)
 access_token = jsonObject['access_token']
 
-
 # Send Voice File
-url = 'http://vop.baidu.com/server_api'
-headers = {'content-type': 'application/json', 'rate': '16000'}
-params = {'dev_pid': 1536, 'cuid': 'baiduyuyin', 'token': access_token}
-requestData = {}
+url = 'http://vop.baidu.com/server_api '
+headers = {'content-type': 'application/json'}
+# params = {'dev_pid': 1536, 'cuid': 'baiduyuyin', 'token': access_token}
+path = "16k.wav"
+with wave.open(path, "rb") as f:
+    # read the wave's format infomation,and return a tuple
+    params = f.getparams()
+    # get the info
+    nchannels, sampwidth, framerate, nframes = params[:4]
+    # Reads and returns nframes of audio, as a string of bytes.
+    str_bytes_data = f.readframes(nframes)
+    # close the stream
+    f.close()
 
-with open('16k.pcm') as todos:
-    for chore in todos:
-        print(chore)
-
-# response = requests.post(url, json=requestData, headers=headers)
-# print(response.text)
+# convert bytes string to base64 format string
+speech = base64.b64encode(str_bytes_data).decode('utf-8')
+requestData = {
+    "format": "pcm",
+    "rate": framerate,
+    "dev_pid": 1536,
+    "channel": nchannels,
+    "token": access_token,
+    "cuid": "baidu_workshop",
+    "len": len(str_bytes_data),
+    "speech": speech
+}
+response = requests.post(url, json=requestData, headers=headers)
+print(response.text)
